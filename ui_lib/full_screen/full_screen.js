d873a14ae86f7dcd11c28fc8b16189b90803cb74
atari-monk <atari.monk1@gmail.com>
2025-01-28 15:15:26 +0100

    refactor: improve error handling and logging in FullScreen class methods

--- ui_lib/full_screen/full_screen.js
+++ ui_lib/full_screen/full_screen.js
@@ -15,12 +15,25 @@ export class FullScreen {
       rootDivClass: 'game-menu',
       divId: 'game-menu-1',
     }
+    this.error = {
+      goFullScreen: 'Error in goFullScreen:',
+      showMenu: 'Error in showMenu:',
+    }
+    this.warn = {
+      requestFullscreen: 'Fullscreen request not supported',
+      overlay: 'Overlay element not found',
+      button: 'Button element not found',
+    }
   }
 
   async goFullScreen() {
-    this.requestFullscreen()
-    this.hide()
-    await this.showMenu()
+    try {
+      this.requestFullscreen()
+      this.hide()
+      await this.showMenu()
+    } catch (error) {
+      logger.error(this.error.goFullScreen, error)
+    }
   }
 
   requestFullscreen() {
@@ -30,30 +43,51 @@ export class FullScreen {
       el.mozRequestFullScreen ||
       el.webkitRequestFullscreen ||
       el.msRequestFullscreen
-    request?.call(el)
+
+    if (request) {
+      request.call(el)
+    } else {
+      logger.warn(this.warn.requestFullscreen)
+    }
   }
 
   hide() {
     const { fsOverlay, hidden } = this.config
-    document.querySelector(fsOverlay).classList.add(hidden)
+    const overlay = document.querySelector(fsOverlay)
+    if (overlay) {
+      overlay.classList.add(hidden)
+    } else {
+      logger.warn(this.warn.overlay)
+    }
   }
 
   async showMenu() {
     const { componentName, rootDivClass, divId } = this.menuConfig
-    await guiContener.loadComponentResources(componentName)
-    guiContener.createInstance(componentName, rootDivClass, divId)
+
+    try {
+      await guiContener.loadComponentResources(componentName)
+      guiContener.createInstance(componentName, rootDivClass, divId)
+    } catch (error) {
+      logger.error(this.error.showMenu, error)
+    }
   }
 
   init() {
-    const { fsOverlayButton, click, initMsg } = this.config
-    this.setButtonClick(fsOverlayButton, click)
+    const { initMsg } = this.config
+
+    this.setButtonClick()
+
     logger.debug(initMsg)
   }
 
-  setButtonClick(fsOverlayButton, click) {
-    document
-      .getElementById(fsOverlayButton)
-      .addEventListener(click, (event) => this.goFullScreen(event))
+  setButtonClick() {
+    const { fsOverlayButton, click } = this.config
+    const button = document.getElementById(fsOverlayButton)
+    if (button) {
+      button.addEventListener(click, (event) => this.goFullScreen(event))
+    } else {
+      logger.warn(this.warn.button)
+    }
   }
 }
 
