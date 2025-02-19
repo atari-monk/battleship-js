33cc130cb2f59393ce7ca7db1d8a8247a0259597
atari-monk <atari.monk1@gmail.com>
2025-02-19 20:42:19 +0100

    refactor(constants): standardize log levels and console colors to uppercase

--- shared_lib_2/constants.js
+++ shared_lib_2/constants.js
@@ -1,21 +1,33 @@
-export const LEVEL = {
-  debug: 'debug',
-  warn: 'warn',
-  error: 'error',
-}
+export const TYPE = Object.freeze({
+  STRING: 'string',
+  BOOL: 'boolean',
+  NULL: 'null',
+  OBJECT: 'object',
+})
 
-export const CONSOLE_COLOR = {
-  cyan: '\x1b[36m',
-  yellow: '\x1b[33m',
-  red: '\x1b[31m',
-  reset: '\x1b[0m',
-}
+export const LEVEL = Object.freeze({
+  DEBUG: 'debug',
+  WARN: 'warn',
+  ERROR: 'error',
+})
+
+export const CONSOLE_COLOR = Object.freeze({
+  CYAN: '\x1b[36m',
+  YELLOW: '\x1b[33m',
+  RED: '\x1b[31m',
+  RESET: '\x1b[0m',
+})
 
 export const EVENT = {
-  mousemove: 'mousemove',
-  mouseenter: 'mouseenter',
-  touchmove: 'touchmove',
-  touchstart: 'touchstart',
-  click: 'click',
-  wheel: 'wheel',
+  MOUSE_MOVE: 'mousemove',
+  MOUSE_ENTER: 'mouseenter',
+  TOUCH_MOVE: 'touchmove',
+  TOUCH_START: 'touchstart',
+  CLICK: 'click',
+  WHEEL: 'wheel',
 }
+
+export const LANGUAGE = Object.freeze({
+  EN: 'en',
+  PL: 'pl',
+})
