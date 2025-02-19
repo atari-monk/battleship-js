a77deaed47b0978464076e50a89cbee685fbdad0
atari-monk <atari.monk1@gmail.com>
2025-02-19 20:42:47 +0100

    refactor(style): create toggle function for managing CSS class states

---
+++ shared_lib_2/style.js
@@ -0,0 +1,16 @@
+import { TYPE } from './constants.js'
+import { ERROR } from './content.js'
+
+export function toggle({ element, cssClass, forceState } = {}) {
+  if (!element || !(element instanceof HTMLElement)) {
+    throw new Error(ERROR.element.invalid)
+  }
+  if (typeof cssClass !== TYPE.STRING || !cssClass.trim()) {
+    throw new Error(ERROR.cssClass.invalid)
+  }
+
+  return element.classList.toggle(
+    cssClass,
+    typeof forceState === TYPE.BOOL ? forceState : undefined
+  )
+}
